3868b246947f3d400cd5482dbad0cc2a5ba30696
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-05 19:14:49 +0100

    installed cra

---
+++ frontend/src/pages/Blogs.jsx
@@ -0,0 +1,11 @@
+import Blog from "../components/Blog/Blog";
+const Blogs = () => {
+  return (
+    <>
+      <h1>You can write your posts in the blogs page!</h1>
+      <Posts />
+    </>
+  );
+};
+
+export default Blogs;
---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Mail us on feedback@geeksforgeeks.org</h1>
+    </div>
+  );
+};
+
+export default Contact;
