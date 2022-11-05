761c4ac2c9202016122399cd9181c4b9b7ce8d8e
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-05 19:20:07 +0100

    cleared frontend boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,7 @@
-import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Blogs from "./pages/Blogs";
-import SignUp from "./pages/SignUp";
-import Contact from "./pages/Contact";
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blogs" element={<Blogs />} />
-        {/* <Route path="" element={<Posts />} />
-        <Route path=":postSlug" element={<Post />} /> */}
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </Router>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
--- frontend/src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-const Contact = () => {
-  return (
-    <div>
-      <h1>Mail us on feedback@geeksforgeeks.org</h1>
-    </div>
-  );
-};
-
-export default Contact;
