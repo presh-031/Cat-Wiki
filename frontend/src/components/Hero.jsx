5fa068b9e55ade8d3a1958a8e4d7200adad5f572
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-06 09:48:52 +0100

    finished styling first section of hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,23 +5,23 @@ import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 const Hero = () => {
   return (
     <div>
-      <div className="border border-red-800 bg-hero-image-sm  bg-cover bg-center bg-no-repeat ">
+      <div className="border border-red-800 bg-hero-image-sm bg-cover bg-center  bg-no-repeat px-[2.9rem] py-[2.1rem] ">
         <div className="text-[#fff]">
           <p className="mb-[.73rem] font-mystery-quest text-[1.4rem] font-normal leading-[1.911rem]">CatWiki</p>
           <p className="mb-[1.82rem] w-[13.92rem] font-montserrat text-[1rem] font-medium leading-[1.219rem]">
             Get to know more about your cat breed
           </p>
-          <div className="flex w-[9.2rem] items-center border border-red-800 bg-white">
+          <div className=" flex w-[9.2rem] items-center rounded-[5.9rem] border border-red-800 bg-white px-[1.3rem] py-[0.8rem]">
             <input
               type="text"
               placeholder="Search"
-              className="w-[100%] rounded-[5.9rem] border-none text-[#291507] placeholder-[#291507] outline-none"
+              className="w-[100%] border-none text-[1.2rem]  leading-[1.5rem] text-[#291507] placeholder-[#291507] outline-none"
             />
-            <ImSearch color="#291507" />
+            <ImSearch color="#291507" className="h-[1rem] w-[1rem] " />
           </div>
         </div>
       </div>
-      <div>
+      <div className="px-[2.9rem] py-[1.8rem]">
         <p>Most Searched Breeds</p>
         <div></div>
         <div>
