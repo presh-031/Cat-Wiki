46415da6a4d4a01ce0a5bd20e7699b6602fb182d
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-06 11:05:26 +0100

    onto marking up reason component

---
+++ frontend/src/components/Reason.jsx
@@ -0,0 +1,25 @@
+const Reason = () => {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reason;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Reason from "../components/Reason";
 
 const Home = () => {
   return (
     <div className="px-[1.8rem] pt-[2.343rem]">
       <Header />
       <Hero />
+      <Reason />
     </div>
   );
 };
