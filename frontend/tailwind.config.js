ad06b6a7267b79d9719fd898ccd9e0a3ca5a0e2a
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-06 09:02:41 +0100

    setup font imports

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "mystery-quest": ["Mystery Quest", "cursive"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
