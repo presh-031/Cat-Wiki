e5dae2638869333d1acac9f16b520f2f79c249bc
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-07 23:58:18 +0100

    now marking up and styling dropdown menu

--- frontend/src/components/MobileDropDown.jsx
+++ frontend/src/components/MobileDropDown.jsx
@@ -1,17 +1,38 @@
 import { useDropDown } from "../contexts/dropDown";
+import { IoIosClose } from "react-icons/io";
+import { CgSearch } from "react-icons/cg";
 
 const MobileDropDown = () => {
   const [dropDown, actions] = useDropDown();
 
   return (
-    <div onClick={actions.setDropDown} className="absolute top-0 right-0 left-0 border border-black">
-      <div>
-        <div>
-          <img src="" alt="" />
+    <div
+      onClick={actions.setDropDown}
+      className="absolute top-0 right-0 left-0 border border border-red-200 border-black bg-white"
+    >
+      <div className="flex justify-end outline">
+        <div className="border border-red-800 ">
+          <IoIosClose size={"2"} className="h-[1.24rem] w-[1.318rem] border border-red-800 " />
         </div>
       </div>
-      <input type="text" />
-      <div>{/* ?Here, well have the divs that contain the suggestions as paragraphs */}</div>
+      <div>
+        <input type="text" />
+        <CgSearch />
+      </div>
+      <div className="px-[1.8rem]">
+        <div className="flex items-center p-[1.91rem]">
+          <p>American Bobtail</p>
+        </div>
+        <div className="flex items-center p-[1.91rem]">
+          <p>American Curl</p>
+        </div>
+        <div className="flex items-center p-[1.91rem]">
+          <p>American Shorthair</p>
+        </div>
+        <div className="flex items-center p-[1.91rem]">
+          <p>American Wirehair </p>
+        </div>
+      </div>
     </div>
   );
 };
