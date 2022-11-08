ba644698fe260f07f9c54cc07349015011edbd21
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-08 08:40:10 +0100

    chore: finished testing base route

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,24 @@
 const express = require("express");
 const app = express();
+
+require("dotenv").config();
+
+// middleware
+
+app.use("/", (req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Listen for requests
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Connected to db & Server listening on port ${port}`);
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.status(200).send({
+    mssg: "welcome to the app",
+  });
+});
