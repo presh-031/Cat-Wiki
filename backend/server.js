44c5f80438a04b7090a744c142bb05974f36b0fd
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-10 10:36:55 +0100

    added route for more images of specific breed

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 require("dotenv").config();
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 // middleware
-
-app.use("/", (req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+app.use(morgan("dev"));
 
 // Listen for requests
 const port = process.env.PORT || 4000;
@@ -26,6 +23,27 @@ app.get("/api", async (req, res) => {
     res.status(200).send(data);
   } catch (err) {
     console.log(err);
-    // return res.status().json();
+    return res.status(500).json([{}]);
+  }
+});
+
+app.get("/api/photos/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const images = [];
+  try {
+    for (let i = 0; i < 8; i++) {
+      const response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`);
+      const data = await response.json();
+      // push data into images array so server responds once
+      images.push(data);
+    }
+
+    res.status(200).send(images);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      mssg: "error getting images",
+    });
   }
 });
