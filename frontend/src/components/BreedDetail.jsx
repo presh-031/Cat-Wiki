d2a6ec6f566fd83e79662f7d0e9ff7e0404a6487
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-10 10:06:43 +0100

    now converting numbers to tiles

--- frontend/src/components/BreedDetail.jsx
+++ frontend/src/components/BreedDetail.jsx
@@ -1,7 +1,17 @@
+import axios from "axios";
+
 import NumberedDetail from "./NumberedDetail";
 
 const BreedDetail = ({ breedInfo }) => {
   console.log(breedInfo.numberedDetails);
+
+  const showOtherPhotos = async () => {
+    for (let i = 0; i < 8; i++) {
+      const { data } = await axios.get("/api/photos/");
+
+      return data;
+    }
+  };
   return (
     <div>
       <p>{breedInfo.name}</p>
@@ -16,9 +26,13 @@ const BreedDetail = ({ breedInfo }) => {
         Life Span: <span> {breedInfo.lifeSpan} years </span>
       </p>
       {breedInfo.numberedDetails.map((detail) => {
-        console.log(detail);
+        // console.log(detail);
         return <NumberedDetail title={detail[0]} value={detail[1]} />;
       })}
+
+      {/* Other Photos */}
+      <p>Other Photos</p>
+      {}
     </div>
   );
 };
--- frontend/src/components/NumberedDetail.jsx
+++ frontend/src/components/NumberedDetail.jsx
@@ -1,7 +1,20 @@
+import "./NumberedDetail.css";
+
 const NumberedDetail = ({ title, value }) => {
+  const numberToTiles = (value) => {
+    const mockArray = [0, 1, 2, 3, 4];
+    return (
+      <div className="number-to-tiles">
+        {mockArray.map((el, index) => (
+          <span key={index} className={el < value ? "shaded-tiles" : "unshaded-tiles"}></span>
+        ))}
+      </div>
+    );
+  };
   return (
     <div>
-      {title}, {value}
+      <span>{title}:</span>
+      <div>{numberToTiles(value)}</div>
     </div>
   );
 };
