53e6a4b01f1b62e054d336d2f28d9eacadb0b143
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-10 21:58:37 +0100

    all breeds now displaying and navigating properly

--- frontend/src/components/BreedPhoto.jsx
+++ frontend/src/components/BreedPhoto.jsx
@@ -1,7 +1,7 @@
 const BreedPhoto = ({ src }) => {
   return (
     <div className="h-[100%]">
-      <img src={src} alt="breed-pic" className="h-[100%] w-[100%]  object-cover" />
+      <img src={src} alt="breed" className="h-[100%] w-[100%]  object-cover" />
     </div>
   );
 };
