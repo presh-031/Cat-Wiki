6dd8170aad837967bea0031e7c7f19b7d984656f
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-11 08:35:00 +0100

    now lazy-loading images

--- frontend/src/components/BreedPhoto.jsx
+++ frontend/src/components/BreedPhoto.jsx
@@ -1,7 +1,10 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
 const BreedPhoto = ({ src }) => {
   return (
     <div className="h-[100%] cursor-pointer transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
-      <img src={src} alt="breed" className="h-[100%] w-[100%]  object-cover" />
+      <LazyLoadImage effect="blur" src={src} alt="breed" className="h-[100%] w-[100%]  object-cover" />
     </div>
   );
 };
